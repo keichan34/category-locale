60bb1115cd3256cc910479e8754380d9bf6e5686
Keita Kobayashi <keita@kbys.me>
2020-07-08 10:40:15 +0900

    Update

--- category-locale.php
+++ category-locale.php
@@ -1,11 +1,17 @@
 <?php
 /*
  * Plugin Name: Category Locale
- * Plugin URI: https://github.com/keichan34/category-locale
+ * Plugin URI: https://keita.blog/category-locale/
+ * License: GPL v3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Author: Keitaroh Kobayashi
+ * Author URI: https://keita.blog/
+ * Requires at least: 4.7
+ * Requires PHP: 5.6
  */
 
 function k_cl_load_locale() {
-  if (is_category("japanese") || has_category("japanese")) {
+  if (is_category("japanese") || (is_singular() && has_category("japanese"))) {
     switch_to_locale("ja");
   }
 }
